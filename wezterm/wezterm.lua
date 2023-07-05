6abc968ce22517f77b755319173487773b0d889f
Rob Patro <rob-p@users.noreply.github.com>
2023-07-05 16:56:12 -0400

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,12 +12,12 @@ end
 
 -- This is where you actually apply your config choices
 config.font = wezterm.font {
-  family = 'JetBrainsMono Nerd Font',
+  family = 'Monofur Nerd Medium',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 }
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Dracula+'
 
 -- and finally, return the configuration to wezterm
 return config
